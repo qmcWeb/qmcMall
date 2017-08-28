f3ea35db352f2f989efb7386bd4859bb6e714dc8
wenjing <15797707647@163.com>
2017-08-28 17:22:35 +0800

    Merge branch 'master' of https://github.com/qmcWeb/qmcMall
    # Conflicts:
    #	src/components/detail/detail.vue

---
+++ src/components/success/success.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="success-wrapper">
+    <div class="success-left left">111</div>
+    <div class="success-right right">222</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "success.styl";
+</style>
