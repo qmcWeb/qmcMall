16befa954ea99eab9b7483b94ce28da7adb773cf
wenjing <15797707647@163.com>
2017-08-31 15:10:53 +0800

    Merge branch 'master' of https://github.com/qmcWeb/qmcMall
    # Conflicts:
    #	src/components/detail/detail.vue

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -16,19 +16,12 @@
           <i class="arrow"></i>
         </li>
         <li class="goods">
-          <router-link :to="{ path: '/goodsList' }">
+          <router-link :to="{ path: '/goodsList',query : {type:'all',price:'all',sort:'default',priceMin:0,priceMax:100000} }" @click="router(item.call,'type',index)">
             全部商品
           </router-link>
-          <a href="#"></a>
           <ul class="list-wrapper">
-            <li class="list">
-              <a href="#">优惠卡券</a>
-            </li>
-            <li class="list">
-              <a href="#">健康美食</a>
-            </li>
-            <li class="list">
-              <a href="#">时尚美妆</a>
+            <li class="list" v-for="(item,index) in type">
+              <a href="javascript:;" @click="router(item.call,'type',index)">{{item.con}}</a>
             </li>
           </ul>
           <i class="arrow"></i>
@@ -78,7 +71,32 @@
 </template>
 
 <script>
-  export default{}
+  export default{
+      data() {
+        return {
+          type: [
+            {con: '优惠卡券', select: false, call: 'card'},
+            {con: '健康美食', select: false, call: 'food'},
+            {con: '时尚美妆', select: false, call: 'fashion'}
+          ],
+          select: {type: '优惠卡券', price: 'all', sort: 'default', priceMin: 0, priceMax: 100000}
+        }
+      },
+      methods: {
+        router(call,selected,index){
+          this.select[selected] = call;
+          this.$router.push({path: '/goodsList',
+            query: {
+              type: this.select.type,
+              price: this.select.price,
+              sort: this.select.sort,
+              priceMin: this.select.priceMin,
+              priceMax: this.select.priceMax
+            }
+          });
+        }
+      }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
