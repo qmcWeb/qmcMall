155a9b14730f77544d26576286b59e506b7739f6
wenjing <15797707647@163.com>
2017-09-12 14:48:00 +0800

    Merge branch 'master' of https://github.com/qmcWeb/qmcMall
    # Conflicts:
    #	src/components/headerMall/headerMall.vue
    #	src/components/login/login.vue

--- src/components/homeVip/homeVip.vue
+++ src/components/homeVip/homeVip.vue
@@ -63,66 +63,11 @@
         <div class="homeVip-title">
           <span class="icon-mall"></span>
           仓豆商城
-          <a class="right see-more">去仓豆商城看看》</a>
+          <a class="right see-more" href="/homeMall">去仓豆商城看看》</a>
         </div>
         <div class="beanAD left shadow"></div>
         <div class="beanShow right">
           <ul class="beanMall-ul">
-            <li class="left">
-              <div class="remove beanMall-li-content shadow">
-                <div class="beanMall-li-item1">
-                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
-                </div>
-                <div class="beanMall-li-item2">
-                  <p class="goodsName">这里是产品名称</p>
-                  <p class="goodsPrice">仓豆：<span>500</span></p>
-                </div>
-              </div>
-            </li>
-            <li class="left">
-              <div class="remove beanMall-li-content shadow">
-                <div class="beanMall-li-item1">
-                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
-                </div>
-                <div class="beanMall-li-item2">
-                  <p class="goodsName">这里是产品名称</p>
-                  <p class="goodsPrice">仓豆：<span>500</span></p>
-                </div>
-              </div>
-            </li>
-            <li class="left">
-              <div class="remove beanMall-li-content shadow">
-                <div class="beanMall-li-item1">
-                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
-                </div>
-                <div class="beanMall-li-item2">
-                  <p class="goodsName">这里是产品名称</p>
-                  <p class="goodsPrice">仓豆：<span>500</span></p>
-                </div>
-              </div>
-            </li>
-            <li class="left">
-              <div class="remove beanMall-li-content shadow">
-                <div class="beanMall-li-item1">
-                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
-                </div>
-                <div class="beanMall-li-item2">
-                  <p class="goodsName">这里是产品名称</p>
-                  <p class="goodsPrice">仓豆：<span>500</span></p>
-                </div>
-              </div>
-            </li>
-            <li class="left">
-              <div class="remove beanMall-li-content shadow">
-                <div class="beanMall-li-item1">
-                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
-                </div>
-                <div class="beanMall-li-item2">
-                  <p class="goodsName">这里是产品名称</p>
-                  <p class="goodsPrice">仓豆：<span>500</span></p>
-                </div>
-              </div>
-            </li>
             <li class="left">
               <div class="remove beanMall-li-content shadow">
                 <div class="beanMall-li-item1">
