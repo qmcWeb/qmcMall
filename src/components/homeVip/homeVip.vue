50d283348a29ef621684e6998be5745339d17434
wenjing <15797707647@163.com>
2017-09-12 14:23:55 +0800

    Merge branch 'master' of https://github.com/qmcWeb/qmcMall
    # Conflicts:
    #	src/components/headerMall/headerMall.vue
    #	src/components/login/login.vue

--- src/components/homeVip/homeVip.vue
+++ src/components/homeVip/homeVip.vue
@@ -1,5 +1,144 @@
 <template>
-
+  <div class="homeVip-wrapper">
+    <!--1.等级与权益-->
+    <div class="homeVip-grade">
+      <div class="homeVip-content">
+        <div class="homeVip-title">
+          <span class="icon-Diamond-1"></span>
+          等级与权益
+        </div>
+      </div>
+    </div>
+    <!--2.成长任务-->
+    <div class="homeVip-growUp">
+      <div class="homeVip-content">
+        <div class="homeVip-title">
+          <span class="icon-task"></span>
+          成长任务
+          <a class="right see-more">更多》</a>
+        </div>
+        <ul class="growUp-ul">
+          <li class="left remove">
+            <div class="growUp-li-item1">
+              <img src="./growUpTask-1.png" alt="成长任务" width="132" height="130">
+            </div>
+            <div class="growUp-li-item2">
+              <p class="taskName">互动任务</p>
+              <p class="taskAward">成长值：<span>100</span></p>
+            </div>
+          </li>
+          <li class="left remove">
+            <div class="growUp-li-item1">
+              <img src="./growUpTask-1.png" alt="成长任务" width="132" height="130">
+            </div>
+            <div class="growUp-li-item2">
+              <p class="taskName">互动任务</p>
+              <p class="taskAward">成长值：<span>100</span></p>
+            </div>
+          </li>
+          <li class="left remove">
+            <div class="growUp-li-item1">
+              <img src="./growUpTask-1.png" alt="成长任务" width="132" height="130">
+            </div>
+            <div class="growUp-li-item2">
+              <p class="taskName">互动任务</p>
+              <p class="taskAward">成长值：<span>100</span></p>
+            </div>
+          </li>
+          <li class="left remove">
+            <div class="growUp-li-item1">
+              <img src="./growUpTask-1.png" alt="成长任务" width="132" height="130">
+            </div>
+            <div class="growUp-li-item2">
+              <p class="taskName">互动任务</p>
+              <p class="taskAward">成长值：<span>100</span></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--3.仓豆商城-->
+    <div class="homeVip-beanMall">
+      <div class="homeVip-content">
+        <div class="homeVip-title">
+          <span class="icon-mall"></span>
+          仓豆商城
+          <a class="right see-more">去仓豆商城看看》</a>
+        </div>
+        <div class="beanAD left shadow"></div>
+        <div class="beanShow right">
+          <ul class="beanMall-ul">
+            <li class="left">
+              <div class="remove beanMall-li-content shadow">
+                <div class="beanMall-li-item1">
+                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
+                </div>
+                <div class="beanMall-li-item2">
+                  <p class="goodsName">这里是产品名称</p>
+                  <p class="goodsPrice">仓豆：<span>500</span></p>
+                </div>
+              </div>
+            </li>
+            <li class="left">
+              <div class="remove beanMall-li-content shadow">
+                <div class="beanMall-li-item1">
+                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
+                </div>
+                <div class="beanMall-li-item2">
+                  <p class="goodsName">这里是产品名称</p>
+                  <p class="goodsPrice">仓豆：<span>500</span></p>
+                </div>
+              </div>
+            </li>
+            <li class="left">
+              <div class="remove beanMall-li-content shadow">
+                <div class="beanMall-li-item1">
+                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
+                </div>
+                <div class="beanMall-li-item2">
+                  <p class="goodsName">这里是产品名称</p>
+                  <p class="goodsPrice">仓豆：<span>500</span></p>
+                </div>
+              </div>
+            </li>
+            <li class="left">
+              <div class="remove beanMall-li-content shadow">
+                <div class="beanMall-li-item1">
+                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
+                </div>
+                <div class="beanMall-li-item2">
+                  <p class="goodsName">这里是产品名称</p>
+                  <p class="goodsPrice">仓豆：<span>500</span></p>
+                </div>
+              </div>
+            </li>
+            <li class="left">
+              <div class="remove beanMall-li-content shadow">
+                <div class="beanMall-li-item1">
+                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
+                </div>
+                <div class="beanMall-li-item2">
+                  <p class="goodsName">这里是产品名称</p>
+                  <p class="goodsPrice">仓豆：<span>500</span></p>
+                </div>
+              </div>
+            </li>
+            <li class="left">
+              <div class="remove beanMall-li-content shadow">
+                <div class="beanMall-li-item1">
+                  <img src="./beanMall-1.png" alt="仓豆商城" width="270" height="210">
+                </div>
+                <div class="beanMall-li-item2">
+                  <p class="goodsName">这里是产品名称</p>
+                  <p class="goodsPrice">仓豆：<span>500</span></p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +146,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "homeVip.styl";
 </style>
 
 
