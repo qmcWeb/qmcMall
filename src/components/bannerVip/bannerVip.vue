8582f6018f5b58c0c6664c4c00f2a6d37b047b0a
wenjing <15797707647@163.com>
2017-09-13 09:40:22 +0800

    Wen.J

--- src/components/bannerVip/bannerVip.vue
+++ src/components/bannerVip/bannerVip.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="bannerVip-wrapper">
-    1111
+    <div class="mask-wrap">
+      <div class="mask-content right">
+        <div class="mask-layer"></div>
+        <div class="mask-login">
+          <div class="login-title">
+            <span class="left userName">Hi,139****0000</span>
+            <a class="right signIn" href="javascript:;">
+              <span class="icon-sign"></span>签到赢福利
+            </a>
+          </div>
+          <div class="login-avatar">
+            <img src="./W-Avatar.png" alt="女头像">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
