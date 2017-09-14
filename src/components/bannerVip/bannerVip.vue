0f92811fd9d548594dfabbdea0f8efac8c5e13b1
wenjing <15797707647@163.com>
2017-09-14 10:19:51 +0800

    Wen.J

--- src/components/bannerVip/bannerVip.vue
+++ src/components/bannerVip/bannerVip.vue
@@ -5,7 +5,7 @@
       <div class="mask-content right">
         <div class="mask-layer"></div>
         <!--1.已经登录-->
-        <div class="mask-login" v-if="false">
+        <div class="mask-login" v-if="true">
           <div class="login-title">
             <span class="left userName">Hi,139****0000</span>
             <a class="right signIn" href="javascript:;">
@@ -81,7 +81,7 @@
           </div>
         </div>
         <!--2.没有登录-->
-        <div class="mask-noLogin" v-if="true">
+        <div class="mask-noLogin" v-if="false">
           <div class="login-title">Hi,欢迎来到钱满仓会员中心！</div>
           <div class="login-avatar">
             <img src="./noLogin-Avatar.png" alt="未登录" width="86" height="86">
